5e2a92a3fc65b844d19a0ab1459355ab81965701
vanshabhishek123-afk <vanshabhishek123@gmail.com>
2026-06-03 13:19:13 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: []
-    }
+    outDir: 'dist'
   }
 })
